a0892984411b718c66bc727629224ef4095cb660
Razaul Hasan Ansari <razaulhasanansari2000@gmail.com>
2021-07-13 19:28:08 +0530

    Functionality Completed with touch support'

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Tile from "./Tile";
 import Cell from "./Cell";
+import GameOverlay from "./GameOverlay";
 import { Board } from "../helper";
 import useEvent from "../hooks/useEvent";
 
 const BoardView = () => {
   const [board, setBoard] = useState(new Board());
+  const [startX, setStartX] = useState(0);
+  const [startY, setStartY] = useState(0);
 
   const handleKeyDown = (event) => {
     if (board.hasWon()) {
@@ -14,9 +17,50 @@ const BoardView = () => {
 
     if (event.keyCode >= 37 && event.keyCode <= 40) {
       let direction = event.keyCode - 37;
-      {
-        console.log(direction);
-      }
+      let boardClone = Object.assign(
+        Object.create(Object.getPrototypeOf(board)),
+        board
+      );
+      let newBoard = boardClone.move(direction);
+      setBoard(newBoard);
+    }
+  };
+
+  const handleTouchStart = (event) => {
+    if (board.hasWon()) {
+      return;
+    }
+
+    if (event.touches.length !== 1) {
+      return;
+    }
+
+    setStartX(event.touches[0].screenX);
+    setStartY(event.touches[0].screenY);
+  };
+
+  const handleTouchEnd = (event) => {
+    if (board.hasWon()) {
+      return;
+    }
+
+    if (event.changedTouches.length !== 1) {
+      return;
+    }
+    var deltaX = event.changedTouches[0].screenX - startX;
+    var deltaY = event.changedTouches[0].screenY - startY;
+    console.log(deltaX, deltaY);
+    var direction = -1;
+
+    if (Math.abs(deltaX) > 3 * Math.abs(deltaY) && Math.abs(deltaX) > 30) {
+      direction = deltaX > 0 ? 2 : 0;
+    } else if (
+      Math.abs(deltaY) > 3 * Math.abs(deltaX) &&
+      Math.abs(deltaY) > 30
+    ) {
+      direction = deltaY > 0 ? 3 : 1;
+    }
+    if (direction !== -1) {
       let boardClone = Object.assign(
         Object.create(Object.getPrototypeOf(board)),
         board
@@ -59,9 +103,15 @@ const BoardView = () => {
           <div>{board.score}</div>
         </div>
       </div>
-      <div className="board">
+      <div
+        className="board"
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+        tabIndex="1"
+      >
         {cells}
         {tiles}
+        <GameOverlay onRestart={resetGame} board={board} />
       </div>
     </div>
   );
